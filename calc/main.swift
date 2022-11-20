d4340c6f980e2d8ac0ede919994d9c014a436782
powcoder <powcoder@163.com>
2022-11-20 23:27:42 +0800

    41889 calc IOS

---
+++ calc/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  calc
+//
+//  Created by Jesse Clark on 12/3/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import Foundation
+
+var args = ProcessInfo.processInfo.arguments
+args.removeFirst() // remove the name of the program
+
+print(Int(args[0])!)
